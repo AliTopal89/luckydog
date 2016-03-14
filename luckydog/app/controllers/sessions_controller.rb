d77b076cb28a01ae9c470801989c94b4f4fad65d
alitopaloglu <ali_t@gwmail.gwu.edu>
2016-03-14 17:15:23 -0400

    simple authentication

---
+++ luckydog/app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+	
+	def new
+	end
+
+	def create
+		user = User.find_by_email(params[:email])
+		# If user exists AND the password is correct
+		if user && user.authenticate(params[:password])
+			# Save the user id inside the browser cookie. This is how we keep the user
+			# logged in when they navigate to around the luckydog site
+			session[:user_id] = user.id
+			redirect_to '/'
+		else
+			# If users login does not work, send thek back to where the login forms are
+			redirect_to '/login'
+		end
+	end
+
+	def destroy
+		session[:user_id] = nil
+		redirect_to '/login'
+	end
+
+end
