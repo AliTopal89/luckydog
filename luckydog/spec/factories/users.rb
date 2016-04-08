6babf9d875d2016aa49652d5e492284799cd5446
alitopaloglu <ali_t@gwmail.gwu.edu>
2016-04-08 16:34:43 -0400

    tests

--- luckydog/spec/factories/users.rb
+++ luckydog/spec/factories/users.rb
@@ -1,7 +1,9 @@
 FactoryGirl.define do
+
   factory :user do
-    name "MyString"
-    email "MyString"
-    password_digest "MyString"
+    name "ali"
+    email "ali_t@gwmail.gwu.edu"
+    password_digest "secret"
+    # password_digest "MyString"
   end
 end
--- luckydog/spec/models/user_spec.rb
+++ luckydog/spec/models/user_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
+require 'spec_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe 'User system:' do
+
+ context 'signup/login:' do
+
+ 	it 'should have no users registered initially.' do
+ 		expect(User.count).to eq(0)
+    end
+
+    it 'should not be logged on initially.' do
+      expect(@current_user).to eq(nil)
+    end
+
+    let(:user) {create(:user)}
+
+    # it 'should have a single registered user.' do
+    #   expect(User.count).to eq(count+1)
+    # end
+    
+ end
+
 end
