cbcd1b1a44534012428caec9c6af3289166be622
alitopaloglu <ali_t@gwmail.gwu.edu>
2016-05-16 17:19:12 -0400

    login 302 status report fix, session wasn't being saved

--- luckydog/app/controllers/sessions_controller.rb
+++ luckydog/app/controllers/sessions_controller.rb
@@ -4,9 +4,9 @@ class SessionsController < ApplicationController
 	end
 
 	def create
-		user = User.find_by_email(params[:email])
+		user = User.find_by(email: params[:session][:email].downcase)
 		# If user exists AND the password is correct
-		if user && user.authenticate(params[:password])
+		if user && user.authenticate(params[:session][:password])
 			# Save the user id inside the browser cookie. This is how we keep the user
 			# logged in when they navigate to around the luckydog site
 			session[:user_id] = user.id
