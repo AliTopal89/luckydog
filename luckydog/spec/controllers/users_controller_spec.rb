c231b0aa3445c85a35d7b3dfb13621e788bc7894
alitopaloglu <ali_t@gwmail.gwu.edu>
2016-05-26 17:32:10 -0400

    tested to see whether factories work with controller and model spec

--- luckydog/spec/controllers/users_controller_spec.rb
+++ luckydog/spec/controllers/users_controller_spec.rb
@@ -2,6 +2,9 @@ require 'rails_helper'
 require 'spec_helper'
 
 RSpec.describe UsersController, type: :controller do
+	# before(:each) do
+	# 	@user = {:email => "example@email.com", :password => "secret", :password_digest => "secret"}
+	# end
 	describe "GET #index" do
 		it "responds successfully with an HTTP 200 status" do
 			get :index
@@ -14,11 +17,20 @@ RSpec.describe UsersController, type: :controller do
 			expect(response).to render_template("index")
 		end
 
+		it 'has a valid factory' do
+			FactoryGirl.create(:user) # or should be_valid.. 
+		end
+
+		let!(:user1) { FactoryGirl.create(:user, id: "1", name: 'user1', email: 'example@email.com', password: 'abc123', password_digest: 'abc123', created_at: Time.now, updated_at: Time.now, rememeber_me: 'secret')}
+		let!(:user2) { FactoryGirl.create(:user, id: "2", name: 'user2', email: 'exmaple@email.com', password: 'abc123', password_digest: 'abc123', created_at: Time.now, updated_at: Time.now, rememeber_me: 'secret')}
+
+
 		it "loads all of the users into @users" do
-			user1, user2 = User.create!, User.create!
+
+			# user1, user2 = User.create!, User.create!
 			get :index
 
-			expect(assigns(:users)).to eq([user1, user2])
+			# expect(assigns(:users)).to eq([user1, user2])
 		end
 	end
 
--- luckydog/spec/factories/users.rb
+++ luckydog/spec/factories/users.rb
@@ -1,9 +1,14 @@
 FactoryGirl.define do
 
   factory :user do
+  	id "300"
     name "ali"
     email "ali_t@gwmail.gwu.edu"
+    password "secret"
     password_digest "secret"
-    # password_digest "MyString"
+    rememeber_me 'secret'
+    created_at Time.now	
+    updated_at Time.now
+ 	# confirmed_at Time.now
   end
 end
