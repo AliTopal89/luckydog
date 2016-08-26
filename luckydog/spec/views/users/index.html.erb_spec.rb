0f37cafe775b95af17635ae6216bd9c95a7df392
alitopaloglu <ali_t@gwmail.gwu.edu>
2016-08-26 07:11:39 -0400

    used rspec html matchers and users/index tests with the gem

--- luckydog/spec/views/users/index.html.erb_spec.rb
+++ luckydog/spec/views/users/index.html.erb_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
-# RSpec.describe "users/index", type: :view do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+RSpec.describe "users/index" do
+	include RSpecHtmlMatchers
+	it 'renders the HTML template' do 
+		render
+
+		expect(view).to render_template(:index)
+		expect(view).to_not render_template(:new)
+	end
+
+	it 'has tags' do
+		expect('<div class="container"</div>').to have_tag('div') 
+		expect('<p>Uber for dogs</p>').to have_tag('p')
+	end
+end
