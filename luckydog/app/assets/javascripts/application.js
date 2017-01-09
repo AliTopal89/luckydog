0394c036a7a8bfbb01c7394e7429acac44788244
alitopaloglu <ali_t@gwmail.gwu.edu>
2017-01-09 16:21:49 -0500

    minor SSL cert change and initial commit of react.js

--- luckydog/app/assets/javascripts/application.js
+++ luckydog/app/assets/javascripts/application.js
@@ -14,20 +14,7 @@
 //= require bootstrap-sprockets
 //= require jquery_ujs
 //= require turbolinks
-//= require zxcvbn.js
-//= require pw-toggle.js
-//= require pw-strength.js
+//= require react
+//= require react_ujs
+//= require components
 //= require_tree .
-// var Acuity = require('acuityscheduling');
-
-// var acuity = Acuity.basic({
-//   userId: '12066368',
-//   apiKey: c9b0b49bfb1a59e3e25444a1d5828f0f
-// });
-
-// acuity.request('appointments', function (err, res, appointments) {
-//   if (err) return console.error(err);
-//   console.log(appointments);
-// });
-
-// $("a:contains('')").css("font-size", "11pt");
