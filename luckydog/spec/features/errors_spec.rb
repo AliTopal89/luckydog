ed31b6faf841ce94a7a8c162ddc721e669c5a035
alitopaloglu <ali_t@gwmail.gwu.edu>
2017-02-05 17:32:30 -0500

    fix failing exception handling tests

--- luckydog/spec/features/errors_spec.rb
+++ luckydog/spec/features/errors_spec.rb
@@ -4,12 +4,12 @@ RSpec.feature "Errors", type: :feature do
   scenario 'user gets a page not found' do
   	visit '/404'
   	expect(page.status_code).to eq 404
-  	expect(page).to have_content('Errors#page_not_found')
+  	expect(page).to have_content('page_not_found')
   end
 
   scenario 'user gets a custom internal server error page' do
   	visit '/500'
   	expect(page.status_code).to eq 500
-  	expect(page).to have_content('Errors#internal_server_error')
+  	expect(page).to have_content('internal_server_error')
   end
 end
