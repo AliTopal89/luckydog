ceab30079648bfa9fa17d2003f5f6ca8ee485042
alitopaloglu <ali_t@gwmail.gwu.edu>
2017-09-14 22:07:20 -0400

    Minor updates to account creation css and updates to errors spec
    Why:
    Accidentally deleted error spec for content expectation however
    changed the expected content to be rendered. Furthermore, the
    css for password creation was way off
    
    How:
    by testing the functionality of thescenario in the only user spec
    realized that the css was way off probably got that way after error
    page creations

--- luckydog/spec/features/errors_spec.rb
+++ luckydog/spec/features/errors_spec.rb
@@ -4,10 +4,12 @@ RSpec.feature "Errors", type: :feature do
   scenario 'user gets a page not found' do
   	visit '/404'
   	expect(page.status_code).to eq 404
+  	expect(page).to have_content('404 sorry wuff')
   end
 
   scenario 'user gets a custom internal server error page' do
   	visit '/500'
   	expect(page.status_code).to eq 500
+  	expect(page).to have_content(/Internal server error don't bark at me not my fault/)
   end
 end
--- luckydog/spec/features/users_spec.rb
+++ luckydog/spec/features/users_spec.rb
@@ -1,13 +1,21 @@
 require 'rails_helper'
 
 RSpec.feature "Users", type: :feature do
-  scenario 'User creates a new account' do 
-  	visit '/signup' 
-
+  
+  def signup
   	fill_in 'Name', :with =>'Ali'
   	fill_in 'Email', :with => 'example@mail.com'
   	fill_in 'Password', :with => 'Password!'
   	fill_in 'Confirmation', :with => 'Password!'
+  end
+
+  scenario 'User creates a new account' do 
+  	visit '/signup'
+
+  	signup
+
+  	expect(page).to have_css('#pswd-strength-txt')
+  	expect(page).to have_css('#pswd-feedback')
 
   	click_button 'Create my account'
 
